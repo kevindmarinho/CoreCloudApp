9f3a0c3b6f5898fe3efd27aba5454afe1a35a9af
kevin marinho <kevindmarinho4321@gmail.com>
2023-05-21 20:15:24 -0400

    Initial Commit

---
+++ CoreCloudApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreCloudApp
+//
+//  Created by kevin marinho on 21/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
